027a989d1696f3e76345481a9e65a92a35d67178
hanz <lha15042002@gmail.com>
2022-10-28 13:55:13 +0700

    edit bai8.py

--- Bài tập/bai8.py
+++ Bài tập/bai8.py
@@ -1,6 +1,7 @@
 #Viết chương trình nhập vào từ bàn phím một dãy số nguyên với độ dài bất kỳ, dãy số nằm trên 1 dòng, các số cách nhau bởi khoảng trắng. Tính tổng của dãy số và hiển thị ra màn hình.#
 dayso = input()
 danhsachgiatri = dayso.split()
-danhSachso = map(int, danhsachgiatri)
+danhSachso = map(int, danhsachgiatri)duhfyqgebfjdjv
 tongdayso = sum(danhSachso)
 print(tongdayso)
+đahâbkjvâvkbk
